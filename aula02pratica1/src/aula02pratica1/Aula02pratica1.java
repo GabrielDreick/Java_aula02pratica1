a70e82e0fe3a45986599d534673f1631ddacf1e9
GabrielDreick <gabriel.dreick+github@gmail.com>
2025-03-10 20:31:00 -0300

    Add files via upload

---
+++ aula02pratica1/src/aula02pratica1/Aula02pratica1.java
@@ -0,0 +1,20 @@
+
+package aula02pratica1;
+
+
+
+
+/**
+ *
+ * @author Gabriel Dreick
+ */
+public class Aula02pratica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("meu primeiro codigo em java...");
+    }
+    
+}
